623d4ff56461624baa5efbb65fe4c3a43347973f
Alisher Zakir <a3zakir@gmail.com>
2019-04-19 15:18:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector("#nested").querySelector(".target");
+}
+
+function increaseRankBy(n) {
+  var list = document.querySelectorAll(".ranked-list");
+  for (var x of list) {
+    x.innerHTML = parseInt(x.innerHTML, 10) * n;
+  }
+  
+}
+
+function deepestChild() {
+  var curr = document.querySelector("#grand-node");
+  var child = curr.querySelector("div");
+  while (child) {
+    curr = child;
+    child = curr.querySelector("div");
+  }
+  return curr;
+}
