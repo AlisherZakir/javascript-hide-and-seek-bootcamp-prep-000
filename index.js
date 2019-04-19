2e954d63899bb63c23176b54cf72166a59a009d1
Alisher Zakir <a3zakir@gmail.com>
2019-04-19 15:17:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector("#nested").querySelector(".target");
+}
+
+function increaseRankBy(n) {
+  var list = document.querySelectorAll(".ranked-list");
+  for (var x of list) {
+    x.innerHTML = parseInt(x.innerHTML, 10) * n;
+  }
+  
+}
+
+function deepestChild() {
+  var curr = document.querySelector("#grand-node");
+  var child = curr.firstChild;
+  console.log(child);
+  while (child) {
+    curr = child;
+    child = curr[0];
+    console.log("loop")
+  }
+  return curr;
+}
